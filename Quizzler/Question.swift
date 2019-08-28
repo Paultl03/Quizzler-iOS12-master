976fae3cc19d97e984271fac635fee68d08abc69
Paultl03 <Plogan39@gmail.com>
2019-08-28 09:22:52 -0700

    Question.swift file created
    Created Question class and initializer for responses.

---
+++ Quizzler/Question.swift
@@ -0,0 +1,23 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Paul Logan III on 8/28/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question{
+    
+    let questionText : String
+    let answer : Bool
+    
+    init(text: String, correctAnswer : Bool ) {
+         questionText = text
+         answer = correctAnswer
+        
+    }
+    }
+    
+
